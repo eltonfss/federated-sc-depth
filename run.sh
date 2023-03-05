e714fdc3c71862fddc2dc8355f96773a7a1856af
Elton F. de S. Soares <eltonfss@gmail.com>
2023-03-05 10:42:51 -0300

    saving experiment results

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 export PYTHONPATH="$PYTHONPATH:$PWD/src"
 CONFIG_DIR="/home/eltons-pc/Configurations/v3/kitti_raw.txt"
 DATASET_DIR="/home/eltons-pc/Datasets/kitti"
-python src/main.py  --config $CONFIG_DIR --dataset_dir $DATASET_DIR --fed_train_num_rounds=3 --fed_train_num_participants=5 --fed_train_num_local_epochs=1 --num_workers=5 --gpu=0 --fed_train_participant_order="sequential" --fed_train_by_drive=1 --fed_train_num_local_train_batches=-1 --fed_train_num_local_val_batches=-1
+python src/main.py  --config $CONFIG_DIR --dataset_dir $DATASET_DIR --fed_train_num_rounds=3 --fed_train_num_participants=5 --fed_train_num_local_epochs=10 --num_workers=5 --gpu=0 --fed_train_participant_order="sequential" --fed_train_by_drive=1 --fed_train_num_local_train_batches=-1 --fed_train_num_local_val_batches=-1
