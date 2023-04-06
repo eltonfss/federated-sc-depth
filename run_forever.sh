3ac172c3a39210fdcadaca313d5bb0fa443706b7
Elton F. de S. Soares <eltonfss@gmail.com>
2023-04-06 08:36:27 -0300

    implementing script to recover from cuda out of memory error automatically

---
+++ run_forever.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+    echo "Usage: ./run_forever.sh <log_filename>"
+    exit 1
+fi
+
+./run.sh >> "$1"
+
+while true
+do
+    if ! pgrep -f "run.sh" > /dev/null
+    then
+        echo "run.sh is stopped. Restarting in 10 seconds ..."
+        sleep 10
+        ./run.sh >> "$1"
+    else
+      sleep 10
+    fi
+done
