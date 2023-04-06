bf8eae1dfcc0f4f2c59c46772a0d3a065fd63ea9
Elton F. de S. Soares <eltonfss@gmail.com>
2023-04-06 08:36:39 -0300

    saving experiment configuration

--- run.sh
+++ run.sh
@@ -1,16 +1,19 @@
+export PYTORCH_CUDA_ALLOC_CONF=garbage_collection_threshold:0.6,max_split_size_mb:128
+echo "PYTORCH_CUDA_ALLOC_CONF=$PYTORCH_CUDA_ALLOC_CONF"
 export PYTHONPATH="$PYTHONPATH:$PWD/src"
+echo "PYTHONPATH=$PYTHONPATH"
 CONFIG_DIR="/home/eltons-pc/Configurations/v3/kitti_raw.txt"
 DATASET_DIR="/home/eltons-pc/Datasets/kitti"
 OUTPUT_DIR="/home/eltons-pc/Logs/federated-sc-depth"
-RESTORE_DIR="$OUTPUT_DIR/27_03_2023_17:56:11"
-MAX_LOCAL_TRAIN_BATCHES=1000
-MAX_LOCAL_VAL_BATCHES=-1
+RESTORE_DIR="$OUTPUT_DIR/03_04_2023_22:35:35"
+MAX_LOCAL_TRAIN_BATCHES=30
+MAX_LOCAL_VAL_BATCHES=30
 PARTICIPANT_SORTING="sequential"
 DISTRIBUTE_DATASET_BY_DRIVE=0
-NUM_ROUNDS=10
-NUM_PARTICIPANTS=10
-FED_TRAIN_NUM_EPOCHS=10
-NUM_WORKERS=5
+NUM_ROUNDS=30
+NUM_PARTICIPANTS=3
+FED_TRAIN_NUM_EPOCHS=3
+NUM_WORKERS=6
 python src/main.py --config $CONFIG_DIR --dataset_dir $DATASET_DIR --fed_train_num_rounds=$NUM_ROUNDS \
 --fed_train_num_participants=$NUM_PARTICIPANTS --fed_train_num_local_epochs=$FED_TRAIN_NUM_EPOCHS \
 --num_workers=$NUM_WORKERS --fed_train_participant_order=$PARTICIPANT_SORTING \
