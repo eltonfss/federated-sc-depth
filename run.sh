e36726fee0f147ad46626befffa34ebc2d2d183a
Elton F. de S. Soares <eltonfss@gmail.com>
2023-05-08 18:58:01 -0300

    checkpoint

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ CONFIG_DIR="/home/eltons-pc/Configurations/v3/kitti_raw.txt"
 DATASET_DIR="/home/eltons-pc/Datasets/kitti"
 OUTPUT_DIR="/home/eltons-pc/Logs/federated-sc-depth"
 #RESTORE_DIR="$OUTPUT_DIR/08_05_2023_14:52:54" # IID
-#RESTORE_DIR="$OUTPUT_DIR/06_05_2023_17:22:08" # BY DRIVE
+RESTORE_DIR="$OUTPUT_DIR/08_05_2023_18:52:34" # BY DRIVE
 MAX_LOCAL_TRAIN_BATCHES=1000
 MAX_LOCAL_VAL_BATCHES=-1
 #PARTICIPANT_SORTING="sequential" #IID
@@ -28,5 +28,5 @@ python src/main.py --config $CONFIG_DIR --dataset_dir $DATASET_DIR --fed_train_n
 --fed_train_by_drive_sort=$DISTRIBUTE_DATASET_BY_DRIVE_SORT \
 --fed_train_num_local_val_batches=$MAX_LOCAL_VAL_BATCHES --fed_train_state_backup_dir=$OUTPUT_DIR --gpu=0 \
 --fed_train_frac_participants_per_round=$FRAC_PARTICIPANTS_PER_ROUND \
-#--fed_train_state_restore_dir=$RESTORE_DIR
+--fed_train_state_restore_dir=$RESTORE_DIR
 
