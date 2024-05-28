497dd092d27ef3d8944c6b520109c3bad8e38822
Elton F. de S. Soares <eltonfss@gmail.com>
2024-05-27 22:48:48 -0300

    checkpoint

--- src/notebooks/DomainAdaptation/comparative_analysis.py
+++ src/notebooks/DomainAdaptation/comparative_analysis.py
@@ -23,36 +23,53 @@ class ComparativeAnalysis(object):
         for metric in self.metrics:
             differences[metric] = None
             if before_adaptation_row[metric] and after_adaptation_row[metric]:
-                differences[metric] = (before_adaptation_row[metric] - after_adaptation_row[metric]) / \
-                                      before_adaptation_row[metric]
+                differences[metric] = (before_adaptation_row[metric] - after_adaptation_row[metric]) / before_adaptation_row[metric]
         return pd.Series(differences)
+    
+    def compute_metric_averages(self, group):
+        after_adaptation_group = group[group['adaptation_state'] == 'after_adaptation']
+        averages = {}
+        for metric in self.metrics:
+            averages[metric] = None
+            if metric in after_adaptation_group.columns:
+                averages[metric] = after_adaptation_group[metric].mean()
+        return pd.Series(averages)
 
-    def plot_diff_prop(self, df, region_type, source_train_dataset, target_train_dataset):
+    def plot_comparison(self, df, region_type, source_train_dataset, target_train_dataset, label):
         melted_df = pd.melt(df, id_vars=['training_method'], var_name='metric', value_name='value')
         plt.figure(figsize=(12, 8))
         sns.barplot(x='metric', y='value', hue='training_method', data=melted_df, palette='Set2')
         plt.title(
-            f'Difference Proportions Across Test Metrics by Training Method for {region_type} regions in {source_train_dataset} to {target_train_dataset} Domain Adaptation Experiment')
+            f'{label} Across Test Metrics by Training Method for {region_type} regions in {source_train_dataset} to {target_train_dataset} Domain Adaptation Experiment')
         plt.xlabel('Metric')
-        plt.ylabel('Difference Proportion')
+        plt.ylabel(label)
         plt.xticks(rotation=45, ha='right')
         plt.tight_layout()
         plt.legend(title='Training Method', loc='upper right')
         plt.show()
+        
+        
+    def plot_metrics_averages(self, metrics, target_region_type=None):
+        return self.plot_metrics(metrics, comparison_function=self.compute_metric_averages, group_columns=['source_train_dataset', 'target_train_dataset', 'training_method', 'test_region_type'], label='Averages', target_region_type=target_region_type)
 
-    def plot_metrics(self, metrics):
+    def plot_metrics_differences(self, metrics, target_region_type=None):
+        return self.plot_metrics(metrics, comparison_function=self.compute_metric_differences, group_columns=['source_train_dataset', 'target_train_dataset', 'training_method', 'test_dataset', 'test_region_type'], label='Difference Proportions', target_region_type=target_region_type)
+        
+    
+    def plot_metrics(self, metrics, comparison_function, group_columns, label, target_region_type=None):
 
+        assert comparison_function, 'Comparison Function must be passed!'
+        assert group_columns, 'Group Columns must be passed!'
         self.metrics = metrics
 
-        # Grouping by the specified columns and applying the function to compute differences
-        result_change_df = self.result_df.groupby(
-            ['source_train_dataset', 'target_train_dataset', 'training_method', 'test_dataset',
-             'test_region_type']).apply(self.compute_metric_differences).reset_index()
+        # Grouping by the specified columns and applying the comparison function
+        result_change_df = self.result_df.groupby(group_columns).apply(comparison_function).reset_index()
+        display(result_change_df)
 
         # Renaming columns for clarity
         renamed_columns = ['source_train_dataset', 'target_train_dataset', 'training_method', 'test_dataset',
                            'test_region_type']
-        renamed_metrics = [f'{metric}_diff_prop' for metric in self.metrics]
+        renamed_metrics = [f'{metric}_comparison' for metric in self.metrics]
         renamed_columns.extend(renamed_metrics)
         result_change_df.columns = renamed_columns
 
@@ -67,9 +84,11 @@ class ComparativeAnalysis(object):
                     continue  # skip
                 scenario_df = scenario_df.drop(columns=['source_train_dataset', 'target_train_dataset'])
                 for region_type in scenario_df['test_region_type'].unique():
+                    if target_region_type and region_type != target_region_type:
+                        continue
                     kitti_to_ddad_by_region_type_df = scenario_df[scenario_df['test_region_type'] == region_type]
                     kitti_to_ddad_by_region_type_df = kitti_to_ddad_by_region_type_df.drop(columns=['test_region_type'])
-                    label_metric = lambda metric: metric.replace('test_', "").replace("_diff_prop", "").replace("_",
+                    label_metric = lambda metric: metric.replace('test_', "").replace("_comparison", "").replace("_",
                                                                                                                 " ").upper()
                     metric_labels = [label_metric(metric) for metric in renamed_metrics]
                     renamed_columns = ['training_method', 'test_dataset']
@@ -84,4 +103,4 @@ class ComparativeAnalysis(object):
                     pivoted_df.columns = [f"{col[1]} ({col[0]})" for col in pivoted_df.columns]
                     pivoted_df = pivoted_df.reset_index()
                     display(pivoted_df)
-                    self.plot_diff_prop(pivoted_df, region_type, source_train_dataset, target_train_dataset)
+                    self.plot_comparison(pivoted_df, region_type, source_train_dataset, target_train_dataset, label)
